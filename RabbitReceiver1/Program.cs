e69228db7bd4b43480913ec75dd8aee4ffffd80b
Orhan Kaya <59208514+trouqle@users.noreply.github.com>
2023-09-23 16:02:26 +0300

    Add files via upload

---
+++ RabbitReceiver1/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri(uriString: "amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Receiver1 App";
+
+IConnection connection = factory.CreateConnection();
+
+IModel channel = connection.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+channel.QueueBind(queueName, exchangeName, routingKey, arguments: null);
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, args) =>
+{
+
+    Task.Delay(TimeSpan.FromSeconds(value:5)).Wait();
+    var body = args.Body.ToArray();
+
+    string message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine(value: $"Message Received: {message}");
+
+    channel.BasicAck(args.DeliveryTag, multiple: false);
+};
+
+string consumerTag = channel.BasicConsume(queueName, autoAck: false, consumer);
+Console.ReadLine();
+
+channel.BasicCancel(consumerTag);
+
+channel.Close();
+connection.Close();
+
+  
---
+++ RabbitSender/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri(uriString: "amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Sender App";
+
+IConnection connection = factory.CreateConnection(); 
+
+IModel channel = connection.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, durable: false, exclusive:false,autoDelete: false, arguments:null);
+channel.QueueBind(queueName, exchangeName, routingKey, arguments:null);
+
+for (int i = 0; i < 60; i++)
+{
+    Console.WriteLine(value: $"Sending message {i}");
+    byte[] messageBodyBytes = Encoding.UTF8.GetBytes(s: $"Message #{i}");
+    channel.BasicPublish(exchangeName, routingKey, basicProperties: null, messageBodyBytes);
+    Thread.Sleep(millisecondsTimeout: 1000);
+}
+
+
+channel.Close();
+connection.Close();
+
